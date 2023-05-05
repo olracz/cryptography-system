be9a1f166c23eecc78e6adff1abcfa97c0a5062f
Carlo Cobey <carlocobey@gmail.com>
2023-05-05 11:28:44 +0800

    Initial commit

---
+++ LevelOne.py
@@ -0,0 +1,38 @@
+class CLevelOneEncryption:
+
+    def __init__(self):
+        self.__Plain_text = ''
+        self.__Prefix = 'Carlo'
+        self.__Suffix = 'Cobey'
+        self.__XOR_Key = 'L'
+        self.__Caesar_shift = 5
+
+    def encrypt(self, input_text):
+        self.__Plain_text = self.__Prefix + input_text + self.__Suffix
+        caesar_text = ""
+        #Perform caesar cipher first
+        for i in range(len(self.__Plain_text)):
+            char = self.__Plain_text[i]
+            # Encrypt uppercase characters
+            if char.isupper():
+                caesar_text += chr((ord(char) + self.__Caesar_shift - 65) % 26 + 65) # plus 5 shift
+            # Encrypt lowercase characters
+            elif char.islower():
+                caesar_text += chr((ord(char) + self.__Caesar_shift - 97) % 26 + 97)
+            # Leave all other characters unchanged
+            else:
+                caesar_text += char
+
+        xor_key = self.__XOR_Key * len(caesar_text)
+        result = []
+        for i, c in enumerate(caesar_text):
+            test1 = i & len(xor_key)
+            test2 = xor_key[i & len(xor_key)]
+            test = ord(xor_key[i & len(xor_key)])
+            c1 = chr(ord(c) ^ ord(xor_key[i & len(xor_key)]))
+            result.append(c1)
+        return "".join(result)
+
+
+
+    #def decrypt(self, input_text):
---
+++ MainController.py
@@ -0,0 +1,41 @@
+from LevelOne import CLevelOneEncryption
+
+
+class CMainController:
+    def __init__(self):
+        self.__Text = ''
+
+    def execute(self):
+        level_one_encryption = CLevelOneEncryption()
+        while True:
+            print('1.Encrypt')
+            print('2.Decrypt')
+            print('3.Exit')
+            user_choices = input('what to do?:')
+            if user_choices == '1':
+                self.handle_user_choices(user_choices, level_one_encryption)
+
+            elif user_choices == '2':
+                self.handle_user_choice(user_choices, level_one_encryption)
+            elif user_choices == '3':
+                break
+            else:
+                continue
+
+    def handle_user_choices(self, user_choice, level_one_encryption):
+        while True:
+            print('\n3.Exit')
+            if user_choice == '1':
+                self.__Text = input('Enter text to encrypt:')
+                e_text = level_one_encryption.encrypt(self.__Text)
+            if user_choice == '2':
+                self.__Text = input('Enter text to decrypt:')
+                e_text = level_one_encryption.encrypt(self.__Text)
+            if self.__Text == '3':
+                exit
+
+
+
+
+main_controller = CMainController()
+main_controller.execute()
