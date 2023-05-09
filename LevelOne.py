f1c1c8a76cca4c766b8f26a0dda0ce3d0c4c5e3b
Carlo Cobey <carlocobey@gmail.com>
2023-05-09 15:17:17 +0800

    modiy caesar cipher that also include special characters and numbers

--- LevelOne.py
+++ LevelOne.py
@@ -10,18 +10,18 @@ class CLevelOneEncryption:
     def encrypt(self, input_text):
         self.__Plain_text = self.__Prefix + input_text + self.__Suffix
         caesar_text = ""
-        #Perform caesar cipher first
+        # Perform caesar cipher first
         for i in range(len(self.__Plain_text)):
             char = self.__Plain_text[i]
             # Encrypt uppercase characters
             if char.isupper():
-                caesar_text += chr((ord(char) + self.__Caesar_shift - 65) % 26 + 65) # plus 5 shift
+                caesar_text += chr((ord(char) + self.__Caesar_shift - 65) % 26 + 65)  # plus 5 shift
             # Encrypt lowercase characters
             elif char.islower():
                 caesar_text += chr((ord(char) + self.__Caesar_shift - 97) % 26 + 97)
-            # Leave all other characters unchanged
+            # Encrypt non alphabet characters
             else:
-                caesar_text += char
+                caesar_text += chr((ord(char) + self.__Caesar_shift) % 128)
 
         xor_key = self.__XOR_Key * len(caesar_text)
         result = []
