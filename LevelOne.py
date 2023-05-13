5d21e2f3bfed5a67e98458d18220d206f926e21a
Carlo Cobey <carlocobey@gmail.com>
2023-05-13 19:46:06 +0800

    Refactored caesar cipher but will revise the algorithm in xor cipher

--- LevelOne.py
+++ LevelOne.py
@@ -12,49 +12,29 @@ class CLevelOneEncryption:
         caesar_text = ""
         result = []
         # Perform caesar cipher first
-        for i in range(len(self.__Plain_text)):
-            char = self.__Plain_text[i]
-            # Encrypt uppercase characters
-            if char.isupper():
-                caesar_text += chr((ord(char) + self.__Caesar_shift - 65) % 26 + 65)  # plus 5 shift
-            # Encrypt lowercase characters
-            elif char.islower():
-                caesar_text += chr((ord(char) + self.__Caesar_shift - 97) % 26 + 97)
-            # Encrypt non alphabet characters
-            else:
-                caesar_text += chr((ord(char) + self.__Caesar_shift) % 128)
+        for char in self.__Plain_text:
+            caesar_text += chr((ord(char) + self.__Caesar_shift))  # plus 5 shift
 
         xor_key = self.__XOR_Key * len(caesar_text)
 
-        for i, c in enumerate(caesar_text):
-            test1 = i & len(xor_key)
-            test2 = xor_key[i & len(xor_key)]
-            test = ord(xor_key[i & len(xor_key)])
-            c1 = chr(ord(c) ^ ord(xor_key[i & len(xor_key)]))
+        for i, char in enumerate(caesar_text):
+            print(xor_key[i & len(xor_key)])
+            c1 = chr(char ^ ord(xor_key[i & len(xor_key)]))
             result += c1
         return result
 
     def decrypt(self, input_text):
         decrypted_xor = ""
-        decrypted_text = ""
+        decrypted_caesar = ""
 
         for i, c in enumerate(input_text):
             c1 = chr(ord(c) ^ ord(self.__XOR_Key[i % len(self.__XOR_Key)]))
             decrypted_xor += c1
 
         for char in decrypted_xor:
-            if char.isupper():
-                decrypted_caesar = chr((ord(char) -  self.__Caesar_shift - 65) % 26 + 65)
-            # Encrypt lowercase characters
-            elif char.islower():
-                decrypted_caesar = chr((ord(char) - self.__Caesar_shift - 97) % 26 + 97)
-            # Encrypt non alphabet characters
-            else:
-                decrypted_caesar = chr((ord(char) - self.__Caesar_shift) % 128)
+            decrypted_caesar = chr(ord(char) - self.__Caesar_shift)
 
-            decrypted_text += decrypted_caesar
-
-        return decrypted_text
+        return decrypted_caesar
 
 
 
