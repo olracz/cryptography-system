11e845cef984e065362a77143f098f635ba0a927
Carlo Cobey <carlocobey@gmail.com>
2023-05-23 02:56:56 +0800

    Revised decryption function but will improve the decryption algorithm

--- LevelOne.py
+++ LevelOne.py
@@ -1,36 +1,40 @@
 class CLevelOneEncryption:
 
-    def __init__(self):
-        self.__Plain_text = ''
-        self.__Prefix = 'Carlo'
-        self.__Suffix = 'Cobey'
-        self.__XOR_Key = 'L'
-        self.__Caesar_shift = 5
+        def __init__(self):
+            self.__Plain_text = ''
+            self.__Prefix = 'Carlo'
+            self.__Suffix = 'Cobey'
+            self.__XOR_Key = 'L'
+            self.__Caesar_shift = 5
 
-    def encrypt(self, input_text):
-        self.__Plain_text = self.__Prefix + input_text + self.__Suffix
-        caesar_text = ""
-        result = []
-        # Perform caesar cipher first
-        for char in self.__Plain_text:
-            caesar_text += chr((ord(char) + self.__Caesar_shift))  # plus 5 shift
+        def encrypt(self, input_text):
+            self.__Plain_text = self.__Prefix + input_text + self.__Suffix
+            caesar_text = ""
+            result = []
 
-        xor_key = self.__XOR_Key * len(caesar_text)
+            for char in self.__Plain_text:
+                caesar_text += chr((ord(char) + self.__Caesar_shift) % 256)
 
-        for i, char in enumerate(caesar_text):
-            c1 = chr((ord(char) ^ ord(xor_key[i & len(xor_key)])))
-            result += c1
-        return result
+            xor_key = self.__XOR_Key * len(caesar_text)
 
-    def decrypt(self, input_text):
-        decrypted_xor = ""
-        decrypted_caesar = ""
+            for i, char in enumerate(caesar_text):
+                c1 = chr((ord(char) ^ ord(xor_key[i & len(xor_key)])))
+                result += c1
 
-        for i, c in enumerate(input_text):
-            c1 = chr(ord(c) ^ ord(self.__XOR_Key[i % len(self.__XOR_Key)]))
-            decrypted_xor += c1
+            print(result)
 
-        for char in decrypted_xor:
-            decrypted_caesar = chr(ord(char) - self.__Caesar_shift)
+        def decrypt(self, input_text):
+            xor_key1 = self.__XOR_Key * len(input_text)
+            caesar_text = ""
 
-        return decrypted_caesar
+            for i, char in enumerate(input_text):
+                c1 = chr(ord(char) ^ ord(xor_key1[i % len(xor_key1)]))
+                caesar_text += c1
+            print(caesar_text)
+            original_text = ""
+            for char in caesar_text:
+                original_text += chr((ord(char) - self.__Caesar_shift) % 256)
+
+            decrypted_text = original_text[len(self.__Prefix):-len(self.__Suffix)]
+            print(decrypted_text)
+            return decrypted_text
