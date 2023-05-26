05d12e43c8aef4951d4cd5a60603036b976afab9
Carlo Cobey <carlocobey@gmail.com>
2023-05-26 17:30:11 +0800

    Revised MainController class and print only encrypted and decrypted output

--- LevelOne.py
+++ LevelOne.py
@@ -1,64 +1,39 @@
-
-import ast
 class CLevelOneEncryption:
+    def __init__(self):
+        self.__Plain_text = ''
+        self.__Prefix = 'Kai-do'
+        self.__Suffix = 'Blackbeard'
+        self.__XOR_Key = 'L'
+        self.__Caesar_shift = 5
 
+    def encrypt(self, input_text):
+        self.__Plain_text = self.__Prefix + input_text + self.__Suffix
 
-        def __init__(self):
-            self.__Plain_text = ''
-            self.__Prefix = 'Kaido'
-            self.__Suffix = 'Blackbeard'
-            self.__XOR_Key = 'L'
-            self.__Caesar_shift = 5
+        caesar_text = ""
+        result = ""
 
-        def encrypt(self, input_text):
-            self.__Plain_text = self.__Prefix + input_text + self.__Suffix
-            print()
-            print("Step 1: Appending prefix and suffix to input text")
-            print("Modified input text :", self.__Plain_text)
-            print()
-            caesar_text = ""
-            result = []
+        for char in self.__Plain_text:
+            caesar_text += chr((ord(char) + self.__Caesar_shift) % 256)
 
-            for char in self.__Plain_text:
-                caesar_text += chr((ord(char) + self.__Caesar_shift) % 256)
-            print("Step 2: Performing Caesar encryption")
-            print("Caesar encrypted text: ", caesar_text)
-            print()
+        xor_key = self.__XOR_Key * len(caesar_text)
 
-            xor_key = self.__XOR_Key * len(caesar_text)
+        for i, char in enumerate(caesar_text):
+            c1 = chr((ord(char) ^ ord(xor_key[i & len(xor_key)])))
+            result += c1
+        return result
 
-            for i, char in enumerate(caesar_text):
-                c1 = chr((ord(char) ^ ord(xor_key[i & len(xor_key)])))
-                result += c1
+    def decrypt(self, input_text):
+        xor_key1 = self.__XOR_Key * len(input_text)
+        caesar_text = ""
 
-            print("Step3 : Performing XOR encryption with key")
-            print("Final encrpted text: ", result)
-            
+        for i, char in enumerate(input_text):
+            c1 = chr(ord(char) ^ ord(xor_key1[i % len(xor_key1)]))
+            caesar_text += c1
 
-        def decrypt(self, input_text):
+        original_text = ""
 
-            user_list = ast.literal_eval(input_text)
-            xor_key1 = self.__XOR_Key * len(user_list)
-            caesar_text = ""
+        for char in caesar_text:
+            original_text += chr((ord(char) - self.__Caesar_shift) % 256)
 
-            for i, char in enumerate(user_list):
-                c1 = chr(ord(char) ^ ord(xor_key1[i % len(xor_key1)]))
-                caesar_text += c1
-            print()
-            print("Step 1 : Returning the value of caesar text before the xor cipher")
-            print("Caesar text : " , caesar_text)
-            print()
-
-            original_text = ""
-
-            for char in caesar_text:
-                original_text += chr((ord(char) - self.__Caesar_shift) % 256)
-            print("Step 2: Returning the value of plain text before caesar cipher")
-            print("Plaintext: ", original_text)
-            print()
-
-            decrypted_text = original_text[len(self.__Prefix):-len(self.__Suffix)]
-            print("Step 3 : Removing the prefix and suffix from plain text")
-            print("Original text: ", decrypted_text)
-            print()
-            return decrypted_text
+        decrypted_text = original_text[len(self.__Prefix):-len(self.__Suffix)]
+        return decrypted_text
--- MainController.py
+++ MainController.py
@@ -1,6 +1,6 @@
 import sys
-
 from LevelOne import CLevelOneEncryption
+
 
 class CMainController:
     def __init__(self):
@@ -12,29 +12,32 @@ class CMainController:
             print('1.Encrypt')
             print('2.Decrypt')
             print('3.Exit')
-            user_choices = input('what to do?:')
+            user_choices = input('what to do?:\n')
             if user_choices == '1':
                 self.handle_user_choices(user_choices, level_one_encryption)
             elif user_choices == '2':
                 self.handle_user_choices(user_choices, level_one_encryption)
-            elif user_choices =='3':
+            elif user_choices == '3':
                 sys.exit()
             else:
-                print("Please enter only from choices")
-                print()
+                print("Please enter only from choices\n")
 
     def handle_user_choices(self, user_choices, level_one_encryption):
         while True:
             if user_choices == '1':
-                self.__Text = input('Enter text to encrypt:')
+                self.__Text = input('Enter text to encrypt:\n')
+                if self.__Text == "":
+                    continue
                 e_text = level_one_encryption.encrypt(self.__Text)
+                print("Encrypted output:\nW", e_text)
                 break
             if user_choices == '2':
-                self.__Text = input('Enter text to decrypt:')
+                self.__Text = input('Enter text to decrypt:\n')
+                if self.__Text == "":
+                    continue
                 e_text = level_one_encryption.decrypt(self.__Text)
-
-
-
+                print("Decrypted output:\n", e_text)
+                break
 
 
 main_controller = CMainController()
